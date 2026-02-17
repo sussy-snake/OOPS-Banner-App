0050a220b27728faea76a701676f4b045d473e46
sussy-snake <jaytriharshvardhan2007@gmail.com>
2026-02-17 09:55:56 +0530

    UC2

--- UC2.java
+++ UC2.java
@@ -1,9 +1,10 @@
-public class UC2;
+public class UC2{
     public static void main(String[] args) {
-        System.out.println("  *     *     *  *  *  *");
-        System.out.println(" * *   * *   *  *  * *");
-        System.out.println("*   *  *   *  *  *  *  *");
-        System.out.println(" * *   * *    *  *  * *");
-        System.out.println("  *     *      * *  *  *");
+        System.out.println("*****   *****   *****   *****");
+        System.out.println("*   *   *   *   *   *   *   *");
+        System.out.println("*   *   *   *   *****   ***");
+        System.out.println("*   *   *   *   *          **");
+        System.out.println("*   *   *   *   *       *   *")
+        System.out.println("*****   *****   *       *****");
     }
 }
